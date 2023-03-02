cb03a8f2d93f1e41bcffc916d41335f4ca871231
ahmederaoui <ahmederaoui1@gmail.com>
2023-03-02 21:23:02 +0100

    Smile application version 1.0

--- app/src/main/java/com/example/smile/FormActivity.java
+++ app/src/main/java/com/example/smile/FormActivity.java
@@ -2,16 +2,33 @@ package com.example.smile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
-
+    private EditText firstName;
+    private EditText lastName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_form);
         String toastText="The application started";
         Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+    }
+
+    public void envoyer(View v){
+        Intent intent=new Intent(FormActivity.this,SmileActivity.class);
+        Bundle bundle= new Bundle();
+        bundle.putString("firstName",firstName.getText().toString());
+        bundle.putString("lastName",lastName.getText().toString());
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/smile/SmileActivity.java
+++ app/src/main/java/com/example/smile/SmileActivity.java
@@ -2,13 +2,33 @@ package com.example.smile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class SmileActivity extends AppCompatActivity {
+    private TextView message;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_smile);
+        Bundle b = getIntent().getExtras();
+        String firstName = b.getString("firstName");
+        String lastName = b.getString("lastName");
+        message = findViewById(R.id.textMessage);
+        String hey = getString(R.string.hi);
+        String sentence=getString(R.string.mes);
+        message.setText(hey+firstName+" "+lastName+sentence);
+    }
+    public void retour(View view){
+        Intent intent = new Intent(SmileActivity.this,FormActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
